04b029f5c70715312673d7aadd5afa93207f6004
virat <virat@gmail.com>
2023-02-14 22:20:40 +0530

    5: Buttons div created

--- src/Button.js
+++ src/Button.js
@@ -4,11 +4,17 @@ import './Button.css'
 function Button(){
     return(
         <div className="bt_main">
-            <div className="bt_col1"></div>
+            <div className="bt_col1">
+                
+            </div>
             <div className="bt_col2"></div>
             <div className="bt_col3"></div>
-            <div className="bt_col4"></div>
-            <div className="bt_col5"></div>
+            <div className="bt_col4">
+                {/* <button className="btEnter"></button>
+                <button className="btClear"></button> */}
+            </div>
+                
+            
         </div>
     );
 }
