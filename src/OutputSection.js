ea14f36a7c49dedb5dbddd60f4b6ad84198881a6
virat <virat@gmail.com>
2023-02-14 22:52:58 +0530

    7:Basic Layout Completed

--- src/OutputSection.js
+++ src/OutputSection.js
@@ -3,8 +3,10 @@ import './OutputSection.css'
 
 function OutputSection(){
     return(
-        <>
-        </>
+        <div className="ot_main">
+            <div className="ot_row1"></div>
+            <div className="ot_row2"></div>
+        </div>
     );
 }
 
