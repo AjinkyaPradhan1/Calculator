621337e3865d6999d42f1b4159e8345f3a2f03e4
virat <virat@gmail.com>
2023-02-14 22:44:15 +0530

    7:Output Screen LAyout Started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Button from './Button';
+import OutputSection from './OutputSection';
 
 function App() {
   return (
     <div className="calc_wrapper">
       <div className='calc_main'>
-        <div className='calc_output_screen'></div>
+        <div className='calc_output_screen'>
+          <Output />
+        </div>
         <div className='calc_button'>
           <Button />
         </div>
---
+++ src/OutputSection.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './OutputSection.css'
+
+function OutputSection(){
+    return(
+        <>
+        </>
+    );
+}
