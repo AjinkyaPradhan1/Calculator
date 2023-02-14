0e587b31274c8cdfe4a9bfc4264679d148c9c417
virat <virat@gmail.com>
2023-02-14 21:41:50 +0530

    3: Basic Layout Creation in Progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Button from './Button';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="calc_wrapper">
+      <div className='calc_main'>
+        <div className='calc_output_screen'></div>
+        <div className='calc_button'>
+          <Button />
+        </div>
+      </div>
     </div>
   );
 }
