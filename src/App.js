3e1971eb15547937f705d033d59dbb56ed7e1077
virat <virat@gmail.com>
2023-02-15 23:16:08 +0530

    Updates for App.js,OutputSection.js Files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
+import { useState } from 'react';
 import Button from './Button';
 import OutputSection from './OutputSection';
 
 function App() {
+  const [text,setText] = useState("Bye");
+  const [result,setResult] = useState("Hi");
+
+  const addToText = (val) => {
+    setText((text)=>[...text,val+" "]);
+  };
   return (
+    // <OutputSection text={text} result={result}/>
     <div className="calc_wrapper">
       <div className='calc_main'>
         <div className='calc_output_screen'>
-          <OutputSection />
+          <OutputSection result={result} text={text}/>
         </div>
         <div className='calc_button'>
-          <Button />
+          <Button /> 
         </div>
       </div>
     </div>
--- src/OutputSection.js
+++ src/OutputSection.js
@@ -1,11 +1,11 @@
 import React from "react";
 import './OutputSection.css'
 
-function OutputSection(){
+const OutputSection = ({text,result}) => {
     return(
         <div className="ot_main">
-            <div className="ot_row1">Hello</div>
-            <div className="ot_row2">Ajinkya</div>
+            <div className="ot_row1">{result}</div>
+            <div className="ot_row2">{text}</div>
         </div>
     );
 }
